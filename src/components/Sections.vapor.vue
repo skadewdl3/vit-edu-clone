67247bcdcf4402a68fadf3204f6eb0c4f86eb98a
Soham Karandikar <sohamk10@gmail.com>
2023-10-16 20:36:50 +0530

    Added section component

--- src/components/Sections.vapor.vue
+++ src/components/Sections.vapor.vue
@@ -6,29 +6,32 @@ console.log(props)
 const sectionsExpanded = ref(Array(props.sections.length).fill(false))
 
 const toggleSection = (index) => {
-  for (let i = 0; i < sectionsExpanded.value.length; i++) {
-    if (i === index) {
-      sectionsExpanded.value[i] = !sectionsExpanded.value[i]
-    } else {
-      sectionsExpanded.value[i] = false
-    }
-  }
+  sectionsExpanded.value[index] = !sectionsExpanded.value[index]
 }
 </script>
 
 <template>
-  <div class="course-sections">
-    <div v-for="(section, index) in props.sections" class="course-section my-4">
+  <div class="course-sections border-solid border-2 border-black/20 rounded-xl">
+    <div v-for="(section, index) in props.sections" class="course-section" :class="{'border-b-2 border-solid border-b-black/20 ': index != sections.length - 1}">
       <div
-        class="course-section-title text-4xl cursor-pointer"
+        class="course-section-title text-3xl cursor-pointer flex items-center justify-between font-bold px-8 py-4"
         @click="toggleSection(index)"
       >
-        {{ section.title }}
+
+      <div class="flex items-center justify-start">
+        <div class="transition-all mr-4" :class="{'rotate-180': sectionsExpanded[index]}">
+          <slot name="icon-down" />
+        </div>
+          <span>{{ section.title }}</span>
       </div>
-      <ul class="course-section-content list-disc" v-if="sectionsExpanded[index]">
+
+      <span class="font-normal text-xl">{{ section.hours ? section.hours + ' hours' : '' }}</span>
+      </div>
+
+      <ul class="course-section-content list-disc px-20 py-4" v-if="sectionsExpanded[index]">
         <li
         v-for="topic in section.topics"
-        class="text-lg"
+        class="text-xl"
         >
         {{ topic }}
       </li>
